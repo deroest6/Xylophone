73d43bb69c1aedb217a2bd1ca35b92644a122308
Jantsen <deroest6@hotmail.com>
2022-05-11 20:11:01 -0600

    Added an opacity effect that dims the button for .2 seconds then returning back to normal

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -15,6 +15,11 @@ class ViewController: UIViewController {
     @IBAction func keyPressed(_ sender: UIButton) {
 
         playSound(soundName: sender.currentTitle!)
+        
+        //Code should execute after 0.2 second delay.
+          DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+              //Bring's sender's opacity back up to fully opaque.
+              sender.alpha = 1.0
     }
     
     func playSound(soundName: String) {
@@ -25,4 +30,4 @@ class ViewController: UIViewController {
 }
 
 
-
+}
